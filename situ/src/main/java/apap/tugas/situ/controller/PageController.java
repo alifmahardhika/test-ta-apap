dfe66a9388e970ad7405bdf802d10916f2ba534f
Muhammad Haikal Baihaqi <hazama254@gmail.com>
2019-11-19 15:37:00 +0700

    Fixed the RoleModel

--- situ/src/main/java/apap/tugas/situ/controller/PageController.java
+++ situ/src/main/java/apap/tugas/situ/controller/PageController.java
@@ -7,6 +7,7 @@ import apap.tugas.situ.service.RoleService;
 import apap.tugas.situ.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -28,6 +29,7 @@ public class PageController {
     public String home(Authentication authentication, Model model) {
 
         model.addAttribute("user", userService.getUser(authentication.getName()));
+      
         return "home";
     }
 
