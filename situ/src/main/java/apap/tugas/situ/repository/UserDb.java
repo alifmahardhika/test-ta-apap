252ab2d7234158d84a2065fb34d608053a495f8a
Alya Isti Safira <safiraalya@gmail.com>
2019-11-20 15:22:55 +0700

    userdb

--- situ/src/main/java/apap/tugas/situ/repository/UserDb.java
+++ situ/src/main/java/apap/tugas/situ/repository/UserDb.java
@@ -5,7 +5,10 @@ import apap.tugas.situ.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserDb extends JpaRepository<UserModel, Long> {
     UserModel findByUsername(String username);
+    Optional<UserModel> findById(Long uuid);
 }
