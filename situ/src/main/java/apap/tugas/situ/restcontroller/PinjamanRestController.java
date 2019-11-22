529aee9623784f98180614ce5c9cced7ccb95c64
Alif Mahardhika <alif.mahardhika@ui.ac.id>
2019-11-22 21:21:51 +0700

    feat/webservice request pinjaman 100%

--- situ/src/main/java/apap/tugas/situ/restcontroller/PinjamanRestController.java
+++ situ/src/main/java/apap/tugas/situ/restcontroller/PinjamanRestController.java
@@ -2,7 +2,11 @@ package apap.tugas.situ.restcontroller;
 
 import apap.tugas.situ.model.Pinjaman;
 import apap.tugas.situ.model.UserModel;
+import apap.tugas.situ.rest.PinjamanResponseDetail;
 import apap.tugas.situ.service.UserService;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.*;
@@ -13,6 +17,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.persistence.Column;
 import javax.validation.Valid;
@@ -31,7 +36,7 @@ public class PinjamanRestController {
     private UserService userService;
 
     @PostMapping(value = "/buat-pinjaman")
-    private Pinjaman createPinjaman(Authentication authentication, @Valid Pinjaman pinjaman, BindingResult bindingResult){
+    private ModelAndView createPinjaman(Authentication authentication, @Valid Pinjaman pinjaman, BindingResult bindingResult, Model model){
         System.out.println("===============disini");
         if (bindingResult.hasFieldErrors()){
             System.out.println("=======================================");
@@ -39,33 +44,45 @@ public class PinjamanRestController {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Request Body has invalid type or missing field");
         }
         else{
-//            HttpHeaders headers = new HttpHeaders();
-//            headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-//            HttpEntity<Pinjaman> entity = new HttpEntity<Pinjaman>(pinjaman,headers);
-//
-//            UserModel user = userService.getUser(authentication.getName());
-//
-//            pinjaman.setIdUser(user.getId());
-//            System.out.println("==========================================");
-//            System.out.println(pinjaman.toString());
-//            return restTemplate.exchange("https://b0e8cbdc-ae36-4281-85c3-be57c1219010.mock.pstmn.io/api/v1/pinjaman/tambah", HttpMethod.POST, entity, String.class).getBody();
 
             UserModel user = userService.getUser(authentication.getName());
-//
+
             pinjaman.setIdUser(user.getId());
-            System.out.println("********************************************");
             System.out.println(pinjaman.getJumlahPinjaman());
-//            UserModel user =  userService.getUser(SecurityContextHolder.getContext().getAuthentication().getName());
-////            pinjaman.setUserId(user.getId());
+
 
             HttpHeaders headers = new HttpHeaders();
             headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
             HttpEntity<Pinjaman> entity = new HttpEntity<Pinjaman>(pinjaman,headers);
             String res = restTemplate.exchange("https://b0e8cbdc-ae36-4281-85c3-be57c1219010.mock.pstmn.io/api/v1/pinjaman/tambah", HttpMethod.POST, entity, String.class).getBody();
 
-            System.out.println("1234567890-234567890-345678904567890-4567890");
+
+            Gson gson = new Gson();
+            JsonParser parser = new JsonParser();
+            JsonObject object = (JsonObject) parser.parse(res);
+
+            PinjamanResponseDetail response = gson.fromJson(object, PinjamanResponseDetail.class);
             System.out.println(res);
-            return pinjaman;
+
+            System.out.println("======================================================================");
+            System.out.println("======================================================================");
+
+            System.out.println(response.getResult().getJumlahPinjaman());
+
+            if (response.getResult().getStatus() == 200){//berhasil
+
+                model.addAttribute("jumlahPinjaman", response.getResult().getJumlahPinjaman());
+                ModelAndView modelAndView = new ModelAndView();
+                modelAndView.setViewName("webservice-pinjaman-berhasil");
+                return modelAndView;
+            }
+            else{//gagal
+                model.addAttribute("jumlahPinjaman", response.getResult().getJumlahPinjaman());
+                ModelAndView modelAndView = new ModelAndView();
+                modelAndView.setViewName("webservice-pinjaman-gagal");
+                return modelAndView;
+
+            }
         }
     }
 }
