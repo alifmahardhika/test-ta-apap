217b82cf0c738e62387767fc0378560b6caf5fca
Alif Mahardhika <alif.mahardhika@ui.ac.id>
2019-11-22 11:03:13 +0700

    feat/cek jumlah pegawai trus buat lowongan pustakawan. webservicenya masih bikin sendiri tapi oke

--- situ/src/main/java/apap/tugas/situ/restcontroller/LowonganRestController.java
+++ situ/src/main/java/apap/tugas/situ/restcontroller/LowonganRestController.java
@@ -12,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDate;
 import java.util.Date;
@@ -37,43 +38,53 @@ public class LowonganRestController {
     UserService userService;
 
     @RequestMapping(value = "/jumlah-pegawai", method = RequestMethod.GET)
-    public String getJumlah(Authentication authentication, Model model){
+    public ModelAndView autoCheckAndAdd(Authentication authentication, Model model){
 
         UserModel user = userService.getUser(authentication.getName());
         JumlahPegawaiDetailResponse jumlahPegawaiDetailResponse;
 
-        if(user.getRole().getId().equals(2L)){
-            jumlahPegawaiDetailResponse = lowonganRestService.getJumlahPegawaiDetail();
-            int jumlah = lowonganRestService.getIntJumlah(jumlahPegawaiDetailResponse);
-            if (jumlah < 5){
-                String closeDate = "";
-                LocalDate dateNow = LocalDate.now();
-                String[] dateStr = dateNow.toString().split("-");
+        jumlahPegawaiDetailResponse = lowonganRestService.getJumlahPegawaiDetail();
+        int jumlah = lowonganRestService.getIntJumlah(jumlahPegawaiDetailResponse);
+        if (jumlah < 5){
+            String closeDate = "";
+            LocalDate dateNow = LocalDate.now();
+            String[] dateStr = dateNow.toString().split("-");
 
-                //cek bulan trus bikin close date
-                if (Integer.parseInt(dateStr[1]) < 12){
+            //cek bulan trus bikin close date
+            if (Integer.parseInt(dateStr[1]) < 12){
 
-                    int monthPlusOne = Integer.parseInt(dateStr[1]) + 1;
-                    dateStr[1] = "" + monthPlusOne;
-                    closeDate = "" + dateStr[0] + "-" +dateStr[1] + "-" + dateStr[2];
-                }
-                else{   //dari desember satu bulan = januari
-                    closeDate = "" + dateStr[0] + "-" +1 + "-" + dateStr[2];
-                }
-                LocalDate closingDate = LocalDate.parse(closeDate);
-                Long idJenisPustakawan = jenisLowonganService.findIdByNama("Pustakawan");
-                LowonganModel automatedCreation = new LowonganModel("Lowongan Pustakawan", dateNow, closingDate, "Dibutuhkan Pustakawan Cakap", 5-jumlah, idJenisPustakawan );
-                System.out.println("================================ni otomatis");
-                System.out.println(automatedCreation.toString());
-                lowonganService.addLowongan(automatedCreation);
+                int monthPlusOne = Integer.parseInt(dateStr[1]) + 1;
+                dateStr[1] = "" + monthPlusOne;
+                closeDate = "" + dateStr[0] + "-" +dateStr[1] + "-" + dateStr[2];
             }
-            model.addAttribute("jumlah", jumlahPegawaiDetailResponse);
-        }
-        else {
-            System.out.println("=============================== lebih dari 5" );
-        }
+            else{   //dari desember satu bulan = januari
+                closeDate = "" + dateStr[0] + "-" +1 + "-" + dateStr[2];
+            }
+            LocalDate closingDate = LocalDate.parse(closeDate);
+            Long idJenisPustakawan = jenisLowonganService.findIdByNama("Pustakawan");
+            LowonganModel automatedCreation = new LowonganModel("Lowongan Pustakawan", dateNow, closingDate, "Dibutuhkan Pustakawan Cakap", 5-jumlah, idJenisPustakawan );
 
-        model.addAttribute("user", user);
-        return "";
+            //            System.out.println("================================ni otomatis");
+//            System.out.println(automatedCreation.toString());
+
+            if (lowonganService.foundSimilar(automatedCreation)){
+                System.out.println("=====================udah ada lowongan dengan jumlah dan judul dan jenis dan tanggal yg sama");
+                model.addAttribute("judulLowongan", automatedCreation.getJudul());
+                ModelAndView modelAndView = new ModelAndView();
+                modelAndView.setViewName("lowongan/add-fail-similar");
+                return modelAndView;
+            }
+            else{
+                lowonganService.addLowongan(automatedCreation);
+                model.addAttribute("judulLowongan", automatedCreation.getJudul());
+                ModelAndView modelAndView = new ModelAndView();
+                modelAndView.setViewName("lowongan/add-lowongan");
+                return modelAndView;
+            }
+        }
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("lowongan/checked-full");
+        return modelAndView;
     }
+
 }
