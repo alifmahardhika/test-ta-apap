54923fdcfc4a7ab81633b30d388706c07bda822d
Alif Mahardhika <alif.mahardhika@ui.ac.id>
2019-12-06 13:04:01 +0700

    resolve gabisa login #4

--- src/main/java/apap/tugas/situ/service/UserServiceImpl.java
+++ src/main/java/apap/tugas/situ/service/UserServiceImpl.java
@@ -6,6 +6,7 @@ import apap.tugas.situ.repository.UserDb;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
