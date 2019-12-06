1b74ca9c8f29f8e746cd6f4382befceaeb526215
Alif Mahardhika <alif.mahardhika@ui.ac.id>
2019-12-06 16:57:54 +0700

    reset repo structure

--- situ/src/main/java/apap/tugas/situ/service/JenisSuratRestService.java
+++
@@ -1,9 +0,0 @@
-package apap.tugas.situ.service;
-
-import apap.tugas.situ.model.JenisSuratModel;
-
-import java.util.Optional;
-
-public interface JenisSuratRestService {
-    JenisSuratModel getJenisSuratById(Long idJenisSurat);
-}
