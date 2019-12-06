a8a6d1a5ceb35588b18a7ab2e2d68dc1900af3c5
Alif Mahardhika <alif.mahardhika@ui.ac.id>
2019-12-06 16:00:16 +0700

    push after resetting db

--- src/main/java/apap/tugas/situ/repository/UserDb.java
+++ src/main/java/apap/tugas/situ/repository/UserDb.java
@@ -1,6 +1,5 @@
 package apap.tugas.situ.repository;
 
-
 import apap.tugas.situ.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
